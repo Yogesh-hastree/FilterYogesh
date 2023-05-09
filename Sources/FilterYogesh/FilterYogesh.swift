52b7b4ec3bde0e093d77717df7c9702485aeda08
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-09 17:39:28 +0530

    check functions working

--- Sources/FilterYogesh/FilterYogesh.swift
+++ Sources/FilterYogesh/FilterYogesh.swift
@@ -2,5 +2,10 @@ public struct FilterYogesh {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print("Hello")
+    }
+    
+    public func msg(str: String){
+        print(str)
     }
 }
