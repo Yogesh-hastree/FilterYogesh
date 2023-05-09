4ca7c4a026535a1daca3d1eb502a7305856a7d3d
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-09 15:27:59 +0530

    Add new class publicly.

---
+++ Sources/FilterYogesh/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hastree on 09/05/23.
+//
+
+import Foundation
+
+public class File{
+    public func massage(str: String){
+        print(str)
+        
+    }
+}
